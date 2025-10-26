927ad70a043f51b41b0ad47070a7f249c23e5bc0
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-10-26 08:27:36 -0300

    Base do Projeto

---
+++ CadastrosRH/src/main/java/com/RHgroup/CadastrosRH/CadastrosRhApplication.java
@@ -0,0 +1,13 @@
+package com.RHgroup.CadastrosRH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastrosRhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrosRhApplication.class, args);
+	}
+
+}
