ce29daf8894e42ee09ccca994c495ad5a1de2f3d
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-10-30 14:41:07 -0300

    Atualização readme

---
+++ src/main/java/com/RHgroup/CadastrosRH/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.RHgroup.CadastrosRH.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
