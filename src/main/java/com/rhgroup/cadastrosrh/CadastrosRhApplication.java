cb70eddf757cbdeff673f7693264f85993d6f41c
AnaVianaCPV <anaviana.cpv@gmail.com>
2025-11-06 17:03:03 -0300

    Aplicando correção em rhgroup/cadastrosrh

--- src/main/java/com/rhgroup/cadastrosrh/CadastrosRhApplication.java
+++ src/main/java/com/rhgroup/cadastrosrh/CadastrosRhApplication.java
@@ -1,11 +1,13 @@
-package com.rhgroup.cadastrosrh;
+package com.RHgroup.CadastrosRH;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CadastrosRhApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(CadastrosRhApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastrosRhApplication.class, args);
+	}
+
 }
